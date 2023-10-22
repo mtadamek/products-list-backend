1db95f8d4e2098b9e78396d6123291f9d5a4186f
Michał <madamek@MacBook-Air-Micha.local>
2023-10-22 18:06:06 +0200

    created get one, post, put and delete method fot products router

--- src/routes/products.ts
+++ src/routes/products.ts
@@ -3,12 +3,54 @@ import { Product } from "../entities/Product";
 import dataSource from "../data-source";
 
 const router = Router();
+const productRepository = dataSource.getRepository(Product);
 
 router.get("/", async (_: Request, res: Response) => {
   try {
-    const productRepository = dataSource.getRepository(Product);
     const products = await productRepository.find();
     res.status(200).send(products);
+  } catch (error) {
+    res.sendStatus(500);
+  }
+});
+
+router.get("/:id", async (req: Request, res: Response) => {
+  try {
+    const { id } = req.params;
+    const product = await productRepository.findOneBy({ id: Number(id) });
+    product ? res.status(200).send(product) : res.sendStatus(404);
+  } catch (error) {
+    res.sendStatus(500);
+  }
+});
+
+router.post("/", async (req: Request, res: Response) => {
+  try {
+    await productRepository.insert(req.body);
+    res.sendStatus(201);
+  } catch (error) {
+    res.sendStatus(500);
+  }
+});
+
+router.put("/:id", async (req: Request, res: Response) => {
+  try {
+    const { id } = req.params;
+    const { affected: isUpdated } = await productRepository.update(
+      id,
+      req.body
+    );
+    isUpdated ? res.sendStatus(200) : res.sendStatus(404);
+  } catch (error) {
+    res.sendStatus(500);
+  }
+});
+
+router.delete("/:id", async (req: Request, res: Response) => {
+  try {
+    const { id } = req.params;
+    const { affected: isDeleted } = await productRepository.delete(id);
+    isDeleted ? res.sendStatus(200) : res.sendStatus(404);
   } catch (error) {
     res.sendStatus(500);
   }
