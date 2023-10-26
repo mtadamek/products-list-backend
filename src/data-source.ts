870869b4897e68ae98d5ca630ab25af1f5da864d
Michał <madamek@MacBook-Air-Micha.local>
2023-10-26 20:22:57 +0200

    Created readme and .env.example

--- src/data-source.ts
+++ src/data-source.ts
@@ -2,13 +2,13 @@ import { DataSource } from "typeorm";
 import { Product } from "./entities/Product";
 import path from "path";
 
-const databasePath = path.resolve(__dirname, "..", "database", "db.sqlite");
+const databasePath = path.resolve(__dirname, "database", "db.sqlite");
 
 const dataSource = new DataSource({
   type: "sqlite",
   database: databasePath,
   synchronize: true,
-  logging: true,
+  logging: false,
   entities: [Product],
   migrations: [],
   subscribers: [],
